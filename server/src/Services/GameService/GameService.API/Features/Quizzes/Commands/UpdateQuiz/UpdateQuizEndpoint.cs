826bfc6aacfef0fb91ffda856d5d01ca85c4e7fd
tmplam <phulam.dev@gmail.com>
2024-12-23 01:00:03 +0700

    implement update quiz endpoint and update postman collection

---
+++ server/src/Services/GameService/GameService.API/Features/Quizzes/Commands/UpdateQuiz/UpdateQuizEndpoint.cs
@@ -0,0 +1,24 @@
+﻿namespace GameService.API.Features.Quizzes.Commands.UpdateQuiz;
+
+public record UpdateQuizRequest(string Name, string Description);
+public record UpdateQuizResponse(Guid QuizId);
+
+public class UpdateQuizEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPatch("/quizzes/{quizId:guid}", async (
+            [FromRoute] Guid quizId,
+            [FromBody] UpdateQuizRequest request,
+            [FromServices] ISender sender) =>
+        {
+            var command = new UpdateQuizCommand(quizId, request.Name, request.Description);
+
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<UpdateQuizResponse>();
+
+            return Results.Ok(ApiResult.Success(response, "Quiz updated"));
+        });
+    }
+}
---
+++ server/src/Services/GameService/GameService.API/Features/Quizzes/Commands/UpdateQuiz/UpdateQuizHandler.cs
@@ -0,0 +1,42 @@
+﻿namespace GameService.API.Features.Quizzes.Commands.UpdateQuiz;
+
+public record UpdateQuizCommand(Guid QuizId, string Name, string Description) : ICommand<UpdateQuizResult>;
+public record UpdateQuizResult(Guid QuizId);
+
+public class CreateQuizCommandValidator : AbstractValidator<UpdateQuizCommand>
+{
+    public CreateQuizCommandValidator()
+    {
+        RuleFor(q => q.Name)
+            .NotEmpty().WithMessage("Quiz name is requied");
+
+        RuleFor(q => q.Description)
+            .NotEmpty().WithMessage("Quiz description is requied");
+    }
+}
+
+
+public class UpdateQuizHandler(
+    IDocumentSession session,
+    IClaimService claimService) 
+    : ICommandHandler<UpdateQuizCommand, UpdateQuizResult>
+{
+    public async Task<UpdateQuizResult> Handle(UpdateQuizCommand command, CancellationToken cancellationToken)
+    {
+        var quiz = await session.LoadAsync<Quiz>(command.QuizId);
+
+        if (quiz == null) throw new NotFoundException("Quiz not found");
+
+        var brandId = Guid.Parse(claimService.GetUserId());
+
+        if (brandId != quiz.BrandId) throw new ForbiddenException();
+
+        quiz.Name = command.Name;
+        quiz.Description = command.Description;
+
+        session.Update(quiz);
+        await session.SaveChangesAsync();
+
+        return new UpdateQuizResult(quiz.Id);
+    }
+}
