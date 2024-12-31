ade8d695e39ea7307c96b5a00cef25543db5337f
tmplam <phulam.dev@gmail.com>
2024-12-31 22:07:36 +0700

    add get image urls endpoint for internal service

---
+++ server/src/Services/MediaService/MediaService.API/Medias/Queries/GetImageUrls/GetImageUrlsEndpoint.cs
@@ -0,0 +1,24 @@
+﻿namespace MediaService.API.Medias.Queries.GetImageUrls;
+
+public record GetImageUrlsRequest(List<Guid> ImageIds);
+public record GetImageUrlsResponse(Dictionary<Guid, string> ImageUrls);
+
+
+public class GetImageUrlsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/medias/image-urls", async (
+            [FromBody] GetImageUrlsRequest request,
+            [FromServices] ISender sender) =>
+        {
+            var query = request.Adapt<GetImageUrlsQuery>();
+
+            var result = await sender.Send(query);
+
+            var response = result.Adapt<GetImageUrlsResponse>();
+
+            return Results.Ok(response);
+        });
+    }
+}
---
+++ server/src/Services/MediaService/MediaService.API/Medias/Queries/GetImageUrls/GetImageUrlsHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace MediaService.API.Medias.Queries.GetImageUrls;
+
+public record GetImageUrlsQuery(List<Guid> ImageIds) : IQuery<GetImageUrlsResult>;
+public record GetImageUrlsResult(Dictionary<Guid, string> ImageUrls);
+
+
+public class GetImageUrlsHandler(IDocumentSession session) : IQueryHandler<GetImageUrlsQuery, GetImageUrlsResult>
+{
+    public async Task<GetImageUrlsResult> Handle(GetImageUrlsQuery query, CancellationToken cancellationToken)
+    {
+        var imageUrls = session
+            .Query<Media>()
+            .Where(media => query.ImageIds.Contains(media.Id))
+            .ToDictionary(media => media.Id, media => media.Url);
+        return await Task.FromResult(new GetImageUrlsResult(imageUrls));
+    }
+}
