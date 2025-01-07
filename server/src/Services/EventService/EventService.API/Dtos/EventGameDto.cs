acfc2ec1b9e58d57999dfd147db2ee888ed8f679
khiemta03 <khiemta.dev@gmail.com>
2025-01-07 23:01:29 +0700

    add start time for event game

--- server/src/Services/EventService/EventService.API/Dtos/EventGameDto.cs
+++ server/src/Services/EventService/EventService.API/Dtos/EventGameDto.cs
@@ -4,5 +4,6 @@ public class EventGameDto
 {
     public string GameId { get; set; } = string.Empty;
     public bool PopUpItemsEnabled { get; set; } = false;
+    public DateTimeOffset? StartTime { get; set; } = null;
     public Guid? QuizzCollectionId { get; set; } = null;
 }
--- server/src/Services/EventService/EventService.API/Entities/EventGame.cs
+++ server/src/Services/EventService/EventService.API/Entities/EventGame.cs
@@ -4,5 +4,6 @@ public class EventGame
 {
     public string GameId { get; set; } = string.Empty;
     public bool PopUpItemsEnabled { get; set; } = false;
+    public DateTimeOffset? StartTime { get; set; } = null;
     public Guid? QuizzCollectionId { get; set; } = null;
 }
