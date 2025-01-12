c239894bc4dab1d54a728383d345f6cdf606cdfb
khiemta03 <khiemta.dev@gmail.com>
2025-01-12 21:01:22 +0700

    add voucher gifted integration

---
+++ server/src/BuildingBlocks/BuildingBlocks.Messaging/IntegrationEvents/VoucherIntegrationEvents/VoucherGiftedIntegrationEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace BuildingBlocks.Messaging.IntegrationEvents;
+
+public class VoucherGiftedIntegrationEvent : IntegrationEvent
+{
+    public Guid VoucherId { get; set; }
+    public Guid SenderId { get; set; }
+    public string SenderEmail { get; set; } = string.Empty;
+    public Guid RecipientId { get; set; }
+    public Guid BrandId { get; set; }
+    public Guid EventId { get; set; }
+    public double Discount { get; set; }
+}
---
+++ server/src/Services/NotificationService/NotificationService.API/Notifications/EventHandlers/IntegrationEventHandlers/VoucherGiftedIntegrationEventHandler.cs
@@ -0,0 +1,34 @@
+﻿namespace NotificationService.API.Notifications.EventHandlers.IntegrationEventHandlers;
+
+public class VoucherGiftedIntegrationEventHandler(
+    ILogger<VoucherGiftedIntegrationEventHandler> _logger,
+    IDocumentSession _session,
+    IHubContext<PlayerNotificationsHub, IPlayerNotificationsClient> _hubContext) : IConsumer<VoucherGiftedIntegrationEvent>
+{
+    public async Task Consume(ConsumeContext<VoucherGiftedIntegrationEvent> context)
+    {
+        _logger.LogInformation("Voucher gifted notification received.");
+
+
+        var notification = new Notification
+        {
+            Id = Guid.NewGuid(),
+            UserId = context.Message.RecipientId,
+            Type = NotificationType.VoucherGifted,
+            Title = "Voucher gifted",
+            Message = $"You have been gifted a {context.Message.Discount}% discount voucher",
+            Data = new Dictionary<string, object>
+            {
+                { "senderId", context.Message.SenderId },
+                { "senderEmail", context.Message.SenderEmail },
+                { "eventId", context.Message.EventId },
+                { "discount", context.Message.Discount },
+            }
+        };
+        _session.Store(notification);
+
+        await Task.WhenAll(
+            _session.SaveChangesAsync(),
+            _hubContext.Clients.User(context.Message.RecipientId.ToString()).ReceivePlayerNotification(notification));
+    }
+}
