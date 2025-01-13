2a99d2550f363cf378f4ebeb5715c34e8433f7d9
ddhuu <ddhuu.dev@gmail.com>
2025-01-14 04:03:22 +0700

    fix time

--- client/mobile/vousphere/lib/data/models/Event.dart
+++ client/mobile/vousphere/lib/data/models/Event.dart
@@ -1,19 +1,18 @@
-import 'package:intl/intl.dart';
 import 'package:vousphere/data/models/Brand.dart';
 
 class Event {
-  String id;
-  String name;
-  String description;
-  String image;
-  String status;
-  DateTime startTime;
-  DateTime endTime;
-  Brand brand;
-  List<Map<String, dynamic>> voucherTypes;
-  int totalVouchers;
-  int totalPublishedVouchers;
-  List<Map<String, dynamic>> games;
+  final String id;
+  final String name;
+  final String description;
+  final String image;
+  final String status;
+  final DateTime startTime;
+  final DateTime endTime;
+  final Brand brand;
+  final List<Map<String, dynamic>> voucherTypes;
+  final int totalVouchers;
+  final int totalPublishedVouchers;
+  final List<Map<String, dynamic>> games;
 
   Event({
     required this.id,
@@ -38,9 +37,11 @@ class Event {
       image: json['image'] ?? '',
       status: json['status'] ?? '',
       startTime:
-          DateTime.parse(json['startTime'] ?? DateTime.now().toIso8601String()),
+          DateTime.parse(json['startTime'] ?? DateTime.now().toIso8601String())
+              .toLocal(),
       endTime:
-          DateTime.parse(json['endTime'] ?? DateTime.now().toIso8601String()),
+          DateTime.parse(json['endTime'] ?? DateTime.now().toIso8601String())
+              .toLocal(),
       brand: Brand.fromJson(json['brand'] ?? {}),
       voucherTypes: List<Map<String, dynamic>>.from(
           json['voucherTypes']?.map((item) => item as Map<String, dynamic>) ??
@@ -50,19 +51,5 @@ class Event {
       games: List<Map<String, dynamic>>.from(
           json['games']?.map((item) => item as Map<String, dynamic>) ?? []),
     );
-  }
-
-  @override
-  String toString() {
-    return 'Event{id: $id, name: $name, description: $description, image: $image, status: $status, startTime: $startTime, endTime: $endTime, brand: $brand, voucherTypes: $voucherTypes, totalVouchers: $totalVouchers, totalPublishedVouchers: $totalPublishedVouchers, games: ${games.map((game) => game.toString()).join(', ')}}';
-  }
-
-  // Method to get startTime of games formatted as HH:mm in UTC+7
-  List<String> getGamesStartTimes() {
-    return games.map((game) {
-      DateTime startTime =
-          DateTime.parse(game['startTime']).add(Duration(hours: 7));
-      return DateFormat('HH:mm').format(startTime);
-    }).toList();
   }
 }
