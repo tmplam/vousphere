b0e52c2cb64b743d10e5dc2909b25d0b21d3274d
21120477 <vuonghuynhkhai4869@gmail.com>
2025-01-13 23:52:38 +0700

    Create QR code

--- client/mobile/vousphere/lib/features/voucher/presentation/dialog/VoucherUsageDialog.dart
+++ client/mobile/vousphere/lib/features/voucher/presentation/dialog/VoucherUsageDialog.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 import 'package:vousphere/data/models/Voucher.dart';
+import 'package:vousphere/features/voucher/presentation/QRPage.dart';
 
-class VoucherUsageDialog extends StatelessWidget {
+class VoucherUsageDialog extends StatefulWidget {
   const VoucherUsageDialog({super.key, required this.voucher});
   final Voucher voucher;
+
+  @override
+  State<VoucherUsageDialog> createState() => _VoucherUsageDialogState();
+}
+
+class _VoucherUsageDialogState extends State<VoucherUsageDialog> {
 
   @override
   Widget build(BuildContext context) {
@@ -18,13 +26,15 @@ class VoucherUsageDialog extends StatelessWidget {
           Row(
             children: [
               Text('Enter code:  '),
-              Text(voucher.code, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.blue.shade600),)
+              Text(widget.voucher.code, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.blue.shade600),)
             ],
           ),
           const SizedBox(height: 10,),
           IconButton(
-              onPressed: () {
-
+              onPressed: () async {
+                bool? status = await Navigator.push(
+                    context,
+                    PageRouteBuilder(pageBuilder: (context, animation, secondaryAnimation) => QRPage(id: widget.voucher.id,),));
               },
               icon: Icon(Icons.qr_code)
           ),
