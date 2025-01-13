24a4efb5bb98fb84db82c6d8deb81bc2d7a07834
tmplam <phulam.dev@gmail.com>
2025-01-14 00:06:02 +0700

    update redeem voucher method

--- server/src/Services/VoucherService/VoucherService.API/Vouchers/Commands/RedeemVoucher/RedeemVoucherEndpoint.cs
+++ server/src/Services/VoucherService/VoucherService.API/Vouchers/Commands/RedeemVoucher/RedeemVoucherEndpoint.cs
@@ -1,4 +1,5 @@
 ﻿using BuildingBlocks.Auth.Constants;
+using BuildingBlocks.Shared;
 using Microsoft.AspNetCore.Mvc;
 
 namespace VoucherService.API.Vouchers.Commands.RedeemVoucher;
@@ -7,14 +8,14 @@ public class RedeemVoucherEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapPost("/api/vouchers/{voucherId:guid}/redeem", async (
+        app.MapGet("/api/vouchers/{voucherId:guid}/redeem", async (
             [FromRoute] Guid voucherId,
             [FromServices] ISender sender) =>
         {
             var command = new RedeemVoucherCommand(voucherId);
             var result = await sender.Send(command);
 
-            return Results.NoContent();
+            return Results.Ok(ApiResult.Success(message: "Redeemed voucher successful"));
         })
             .RequireAuthorization(AuthPolicy.Player);
     }
