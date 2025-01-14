485db4c9408ab40d13cdfb43f9a699e04205abfc
tmplam <phulam.dev@gmail.com>
2025-01-14 10:10:04 +0700

    add event weekly

---
+++ server/src/Services/EventService/EventService.API/Dtos/DayEventStatusDto.cs
@@ -0,0 +1,9 @@
+﻿namespace EventService.API.Dtos;
+
+public class DayEventStatusDto
+{
+    public DateTimeOffset Date { get; set; }
+    public int NumberOfPendings { get; set; }
+    public int NumberOfHappenings { get; set; }
+    public int NumberOfEndeds { get; set; }
+}
---
+++ server/src/Services/EventService/EventService.API/Events/Queries/GetWeekEventStatistics/GetWeekEventStatisticsEndpoint.cs
@@ -0,0 +1,21 @@
+﻿namespace EventService.API.Events.Queries.GetWeekEventStatistics;
+
+public record GetWeekEventStatisticsRequest(DateTimeOffset CurrentDate);
+
+public class GetWeekEventStatisticsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/api/events/week-status", async (
+            [AsParameters] GetWeekEventStatisticsRequest request,
+            [FromServices] ISender sender) =>
+        {
+            var query = request.Adapt<GetWeekEventStatisticsQuery>();
+
+            var result = await sender.Send(query);
+
+            return Results.Ok(ApiResult.Success(result.WeekEventStatus));
+        })
+            .RequireAuthorization(AuthPolicy.Admin);
+    }
+}
